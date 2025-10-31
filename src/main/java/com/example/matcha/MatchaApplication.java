af2eb6101dd521e926be49986052e9859780aec8
user <user@example.com>
2025-10-31 13:56:22 +0900

    henko

--- src/main/java/com/example/matcha/MatchaApplication.java
+++ src/main/java/com/example/matcha/MatchaApplication.java
@@ -2,8 +2,6 @@ package com.example.matcha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 
 import com.example.matcha.entity.Product;
