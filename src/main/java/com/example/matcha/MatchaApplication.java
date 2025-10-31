6a11745920dcfbf6e6d638f2e364e46a65b9f643
user <user@example.com>
2025-10-31 14:27:23 +0900

    henko

--- src/main/java/com/example/matcha/MatchaApplication.java
+++ src/main/java/com/example/matcha/MatchaApplication.java
@@ -2,12 +2,11 @@ package com.example.matcha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// 初期データ投入に関係する不要なインポートやBeanの定義はすべて削除済み
 
 @SpringBootApplication
 public class MatchaApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MatchaApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(MatchaApplication.class, args);
+	}
 }
