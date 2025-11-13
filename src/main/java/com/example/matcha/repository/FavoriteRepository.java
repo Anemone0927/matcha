469081ff40c719ed73922cca2d04c5d0e15a9dea
user <user@example.com>
2025-11-13 09:18:49 +0900

    henko

--- src/main/java/com/example/matcha/repository/FavoriteRepository.java
+++ src/main/java/com/example/matcha/repository/FavoriteRepository.java
@@ -3,9 +3,14 @@ package com.example.matcha.repository;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import com.example.matcha.entity.Favorite;
 import com.example.matcha.entity.Product;
 
