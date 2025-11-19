9a93ecfa8619e9e250cb5720c2bcba06ad311824
user <user@example.com>
2025-11-19 09:38:54 +0900

    henko

--- src/main/java/com/example/matcha/controller/CartController.java
+++ src/main/java/com/example/matcha/controller/CartController.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
